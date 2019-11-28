1f7ffb0d5c268edbe056b4cc44e72f9233e00311
André Gaul <andre@gaul.io>
2019-11-28 19:17:21 +0100

    add quorum intersection experiments

---
+++ experiments/quorum_intersection/run_quorum_intersection_2.py
@@ -0,0 +1,21 @@
+'''Get quorum intersection timings for floor(2/3*n)+1 threshold'''
+
+import math
+
+from experiments.fbas import get_hierarchical_base_definition
+from experiments.quorum_intersection.intersection import intersection
+from experiments.quorum_intersection.plot import plot_quorum_intersection
+
+def main():
+    '''Run experiment'''
+    n_orgs_range, t_elapsed = list(range(3, 11)), []
+    for n_orgs in n_orgs_range:
+        base_definition = get_hierarchical_base_definition(n_orgs, math.floor(2/3*n_orgs) + 1, 3, 2)
+        result, elapsed = intersection(base_definition)
+        assert result is True
+        t_elapsed.append(elapsed)
+
+    plot_quorum_intersection(n_orgs_range, t_elapsed)
+
+if __name__ == "__main__":
+    main()
